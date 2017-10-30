2221a4781a851f6e11dcdb313e21228b95ff4ae4
pedro <petzichila@gmail.com>
2017-10-30 12:01:26 +0000

    Add a comment to project.py

--- project.py
+++ project.py
@@ -1,12 +1,13 @@
 from struct import Node, Edge, Graph
 
-        
 text_file = open("the_mysterious_affair_at_styles.txt", 'r')
 text = text_file.read()
 text = text.lower()
 
-g = Graph(text, 3, 5)
+
+g = Graph(text, 3, 4) # Graph(text, n, D)
 g.symmetric_window()
+#g.nonsymmetric_window()
 g.export("graph.txt")
         
 
