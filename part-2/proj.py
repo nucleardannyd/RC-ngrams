43af0b564027f716d1c9233d796455c74ae93c20
nucleardannyd <daniel.leitao@ist.utl.pt>
2017-12-03 18:10:09 +0000

    Added Acquaintances mode

--- part-2/proj.py
+++ part-2/proj.py
@@ -3,11 +3,24 @@ import sys
 import random
 
 
+def infect(node):
+	for k in range(b):
+		r = random.choice(node.edges)
+		if r.id in suceptible.keys():
+			infected[r.id] = suceptible[r.id]
+			del(suceptible[r.id])	
+	
+    
 if len(sys.argv) < 4:
     print("Script must have 4 arguments.")
     print("python proj.py number_of_infected_acquaintances number_of_days_until_recovery fraction_of_population_required")
     exit(1)
 
+if len(sys.argv) == 5:
+	initInfect = int(sys.argv[4])
+else:
+	initInfect = 0
+	
 snapGraph = snap.LoadEdgeList(snap.PNGraph, "soc-Epinions1.txt")
 rand = snap.TRnd(42)
 rand.Randomize()
@@ -32,36 +45,40 @@ nInfected = 0.0
 suceptible = nodes
 infected = {}
 recovered = {}
+t = 0
 
-for i in range(b):
-    rid = snapGraph.GetRndNId()
-    if rid in suceptible.keys():
-        infected[rid] = suceptible[rid]
-        del(suceptible[rid])
-
+if initInfect != 0:
+	for i in range(initInfect):
+		rid = snapGraph.GetRndNId()
+		if rid in suceptible.keys():
+			infected[rid] = suceptible[rid]
+			del(suceptible[rid])
+	
+else:
+	rid = snapGraph.GetRndNId()
+	rids = infect(suceptible[rid])
+	
 t = 1
 
 while fraction < f:
-    inf = infected.keys()
-    for i in inf:
-        if infected[i].daysInfected == j:
-            recovered[i] = infected[i]
-            del(infected[i])
-        else:
-            for k in range(b):
-                if len(infected[i].edges) > 0:
-                    rid = random.choice(infected[i].edges)
-                    if rid.id in suceptible.keys():
-                        infected[rid.id] = rid
-                        del(suceptible[rid.id])
-            infected[i].daysInfected += 1
+	inf = infected.keys()
+	print("Nodes infected: " + str(len(inf)))
+	for i in inf:
+		if infected[i].daysInfected == j:
+			recovered[i] = infected[i]
+			del(infected[i])
+		else:
+			if len(infected[i].edges) > 0:
+				rids = infect(infected[i])
+				infected[i].daysInfected += 1
 
-    nInfected = float(len(infected.keys()) + len(recovered.keys()))
-    fraction = float(nInfected / n)
-    print (nInfected)
-    print (fraction)
-    print (n)
-    t += 1
+	nInfected = float(len(infected.keys()) + len(recovered.keys()))
+	fraction = float(nInfected / n)
+	print (nInfected)
+	print (fraction)
+	print("Infected: " + str(len(infected)))
+	print("recovered: " + str(len(recovered)))
+	t += 1
 
 print("It took " + str(t) + " days for " +
       str(f * 100) + "% of people to be infected")
