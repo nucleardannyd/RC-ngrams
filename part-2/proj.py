35c76b19a8468184ff446fbf00f38d6f8939fc96
nucleardannyd <daniel.leitao@ist.utl.pt>
2017-12-03 20:21:02 +0000

    changes

--- part-2/proj.py
+++ part-2/proj.py
@@ -2,37 +2,38 @@ from graph import *
 import sys
 import random
 
-
 def infect(node):
+	repeated = 0
 	for k in range(b):
-		r = random.choice(node.edges)
-		if r.id in suceptible.keys():
-			infected[r.id] = suceptible[r.id]
-			del(suceptible[r.id])	
-	
+		if len(node.edges) != 0:
+			r = random.choice(node.edges)
+			if r.id in suceptible.keys():
+				infected[r.id] = suceptible[r.id]
+				del(suceptible[r.id])	
+			elif r.id in infected.keys():
+				repeated += 1
+	return repeated
     
-if len(sys.argv) < 4:
-    print("Script must have 4 arguments.")
-    print("python proj.py number_of_infected_acquaintances number_of_days_until_recovery fraction_of_population_required")
+if len(sys.argv) < 5:
+    print("Script must have 5 arguments.")
+    print("python proj.py number_of_infected_acquaintances number_of_days_until_recovery number_of_initial_infected_nodes function_to_use")
     exit(1)
 
-if len(sys.argv) == 5:
-	initInfect = int(sys.argv[4])
-else:
-	initInfect = 0
 	
 snapGraph = snap.LoadEdgeList(snap.PNGraph, "soc-Epinions1.txt")
-rand = snap.TRnd(42)
-rand.Randomize()
-
 
 n = snapGraph.GetNodes()
 b = int(sys.argv[1])
 j = int(sys.argv[2])
-f = float(sys.argv[3])
+initInfect = int(sys.argv[3])
+function = sys.argv[4]
 t = 1
-fraction = 0.0
+rand = snap.TRnd(42)
+rand.Randomize()
+#fraction = 0.0
 
+if initInfect > n:
+	initInfect = n
 nodes = {}
 for NI in snapGraph.Nodes():
     nodes[NI.GetId()] = Node(NI.GetId())
@@ -41,44 +42,56 @@ for EI in snapGraph.Edges():
     nodes[EI.GetSrcNId()].addEdge(nodes[EI.GetDstNId()])
 
 
-nInfected = 0.0
+
 suceptible = nodes
 infected = {}
 recovered = {}
 t = 0
-
-if initInfect != 0:
+repeated = 0
+if function == "r":
 	for i in range(initInfect):
-		rid = snapGraph.GetRndNId()
-		if rid in suceptible.keys():
-			infected[rid] = suceptible[rid]
-			del(suceptible[rid])
+		random.choice(suceptible)
+		infected[rid] = suceptible[rid]
+		del(suceptible[rid])
+			
 	
-else:
-	rid = snapGraph.GetRndNId()
-	rids = infect(suceptible[rid])
-	
-t = 1
+elif function == "a":
+	repeateda = 0
+	print(initInfect)
+	for i in range(initInfect):
+		rid = random.choice(suceptible.keys())
+		repeated += infect(suceptible[rid])
+			
+#fraction = float(nInfected / n)
 
-while fraction < f:
+if len(infected.keys()) == n:
+	print("It took " + str(t) + " days for " +
+		str(f * 100) + "% of people to be infected with cost " + str(initInfect) + "and " + str(b) + " neighbours infected for day")
+	
+#print (fraction)
+print("Infected: " + str(len(infected)))
+print("Repeated Nodes " + str(repeated))
+
+
+while len(infected.keys()) < n:
+	t += 1
 	inf = infected.keys()
-	print("Nodes infected: " + str(len(inf)))
+	repeated = 0
 	for i in inf:
 		if infected[i].daysInfected == j:
 			recovered[i] = infected[i]
 			del(infected[i])
 		else:
 			if len(infected[i].edges) > 0:
-				rids = infect(infected[i])
+				repeated += infect(infected[i])
 				infected[i].daysInfected += 1
 
-	nInfected = float(len(infected.keys()) + len(recovered.keys()))
-	fraction = float(nInfected / n)
-	print (nInfected)
-	print (fraction)
+	nInfected = len(infected.keys()) + len(recovered.keys())
+	#fraction = float(nInfected / n)
+	#	print (fraction)
+	print("Day: " + str(t))
 	print("Infected: " + str(len(infected)))
 	print("recovered: " + str(len(recovered)))
-	t += 1
+	print("Repeated Nodes " + str(repeated))
 
-print("It took " + str(t) + " days for " +
-      str(f * 100) + "% of people to be infected")
+print("It took " + str(t) + " days for people to be infected with " + str(initInfect) + " nodes infected")
